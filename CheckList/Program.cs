262b6f68e9a586cefef6ae1452e945d15b24a018
Sam Lindfors <sam.lindfors@elev.ga.ntig.se>
2023-12-19 10:21:19 +0100

    Project DOne i Think

--- CheckList/Program.cs
+++ CheckList/Program.cs
@@ -1,23 +1,4 @@
-﻿
-
-Task.tasks = new List<Task>();
-Task.tasks.Add(new Task("task1", "", DateTime.Now));
-Task.tasks.Add(new Task("task2", "", DateTime.Now));
-Task.tasks.Add(new Task("task3", "", DateTime.Now));
-Task.tasks.Add(new Task("task4", "", DateTime.Now));
-Task.tasks.Add(new Task("task5", "", DateTime.Now));
-Task.tasks.Add(new Task("task6", "", DateTime.Now));
-Task.tasks.Add(new Task("task7", "", DateTime.Now));
-Task.tasks.Add(new Task("task8", "", DateTime.Now));
-Task.tasks.Add(new Task("task9", "", DateTime.Now));
-Task.tasks.Add(new Task("task10", "", DateTime.Now));
-Task.tasks.Add(new Task("task11", "", DateTime.Now));
-Task.tasks.Add(new Task("task12", "", DateTime.Now));
-Task.tasks.Add(new Task("task13", "", DateTime.Now));
-Task.tasks.Add(new Task("task14", "", DateTime.Now));
-Task.tasks.Add(new Task("task15", "", DateTime.Now));
-Task.tasks.Add(new Task("task16", "", DateTime.Now));
-Task.tasks.Add(new Task("task17", "", DateTime.Now));
+﻿Task.tasks = new List<Task>();
 
 while (true)
 {
@@ -29,21 +10,57 @@ while (true)
     string input = Console.ReadLine() ?? "";
     if (input == "1")
     {
+        Console.WriteLine("\nPress the number of the task or any key to go back");
+        LookingAtTasks();
+        input = Console.ReadLine() ?? "";
+        int inputAsInt;
+        int.TryParse(input, out inputAsInt);
+        Console.WriteLine("");
+        if (1 <= inputAsInt && inputAsInt <= Task.tasks.Count)
+        {
+            Console.WriteLine("Do you want to complete(c) or delete(d) this task? Press any key to cancel");
+            Task.WriteTask(Task.tasks[inputAsInt - 1]);
+
+            input = Console.ReadLine() ?? "";
+            if (input.ToUpper() == "C")
+            {
+                Console.WriteLine("Completed well done");
+                Console.Beep();
+                Task.tasks.RemoveAt(inputAsInt - 1);
+            }
+            else if (input.ToUpper() == "D")
+            {
+                Console.WriteLine("Deleted");
+                Task.tasks.RemoveAt(inputAsInt - 1);
+            }
+            Thread.Sleep(1000);
+            Console.Clear();
+        }
+        else continue;
 
     }
     else if (input == "2")
     {
-
+        Task.tasks.Add(Task.AddTask());
     }
     else if (input == "3")
     {
+        Console.Clear();
         return;
     }
-    else 
+    else
+    {
         Console.WriteLine("That is not an option");
+        Console.Clear();
+    }
 
-    Console.Clear();
 }
+
+void LookingAtTasks()
+{
+    Task.WriteOutAllTasks();
+}
+
 
 
 
--- CheckList/Task.cs
+++ CheckList/Task.cs
@@ -8,28 +8,56 @@ public class Task
 
     public string taskName { get; set; }
     public string taskBody { get; set; }
-    public DateTime timeToBeDone { get; set; }
-    public Task(string taskName, string taskBody, DateTime timeToBeDone)
+    public Task(string taskName, string taskBody)
     {
         this.taskName = taskName;
         this.taskBody = taskBody;
-        this.timeToBeDone = timeToBeDone;
+    }
+    public Task() { }
+
+    public static Task AddTask()
+    {
+        Task task = new Task();
+        while (true)
+        {
+            Console.WriteLine("What is the title of the task");
+            string input = Console.ReadLine() ?? "";
+            if (input.Length < 3)
+            {
+                Console.WriteLine("To short");
+            }
+            else if (input.Length > 20)
+            {
+                Console.WriteLine("To Long");
+            }
+            else
+            {
+                task.taskName = input;
+                break;
+            }
+        }
+        Console.WriteLine("What is the body of the task");
+        task.taskBody = Console.ReadLine() ?? "";
+        
+        Console.WriteLine("Task is added");
+        Thread.Sleep(2000);
+        Console.Clear();
+
+        return task;
+
     }
 
     public static void WriteOutAllTasks()
     {
         for (int i = 0; i < tasks.Count; i++)
         {
-            Console.WriteLine(tasks[i].taskName);
+            Console.WriteLine((i+1).ToString() + ": " + tasks[i].taskName);
         }
     }
-
-
     public static void WriteTask(Task task)
     {
         Console.WriteLine(task.taskName + ":");
         Console.WriteLine("     " + task.taskBody);
-        Console.WriteLine("     " + task.timeToBeDone);
     }
 
 }
